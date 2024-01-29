d781e72040e89e4871ab9eba014ddb78668bc8cd
Poir <ckswo00@naver.com>
2024-01-29 17:12:06 +0900

    패키지 이름 수정 및 생성

--- src/main/java/gg/together/chat/repository/BoardRepository.java
+++ src/main/java/gg/together/chat/repository/BoardRepository.java
@@ -3,11 +3,11 @@ package gg.together.chat.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import gg.together.chat.domain.Board;
+import gg.together.chat.domain.BoardEntity;
 import java.util.List;
 
 
 @Repository
-public interface BoardRepository extends JpaRepository<Board, Integer>{
-    List<Board> findByTitleContaining(String keyword);
+public interface BoardRepository extends JpaRepository<BoardEntity, Integer>{
+    List<BoardEntity> findByTitleContaining(String keyword);
 }
